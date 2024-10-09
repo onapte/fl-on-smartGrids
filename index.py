4254c316816bc19d26269ad9b0790f398f296721
Onkar Apte <97520729+onapte@users.noreply.github.com>
2024-10-09 12:22:42 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,12 @@
-# Main file which does computation
+from flask import Flask
+from flwr.server import start_server
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Server is running!"
+
+if __name__ == "__main__":
+    app.run(port=5000)
+    start_server(server_address="localhost:8080", config={"num_rounds": 7})
