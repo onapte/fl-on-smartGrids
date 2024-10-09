8426ab304b491e274486f82085b7cffc566d869b
Onkar Apte <97520729+onapte@users.noreply.github.com>
2024-10-09 12:28:38 -0600

    Update Testing.py

--- Stages/Testing.py
+++ Stages/Testing.py
@@ -1,1 +1,48 @@
+import flwr as fl
+import torch
+import torch.nn as nn
+from util import get_test_data_loader
 
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = nn.functional.relu(self.conv1(x))
+        x = nn.functional.max_pool2d(x, kernel_size=2)
+        x = nn.functional.relu(self.conv2(x))
+        x = nn.functional.max_pool2d(x, kernel_size=2)
+        x = x.view(-1, 64 * 7 * 7)
+        x = nn.functional.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+def evaluate_model(model):
+    test_loader = get_test_data_loader(batch_size=32)
+    model.eval()
+    test_loss = 0
+    correct = 0
+    criterion = nn.CrossEntropyLoss()
+
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+            test_loss += criterion(output, target).item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    accuracy = correct / len(test_loader.dataset)
+    return test_loss, accuracy
+
+def test_model():
+    model = SimpleCNN()
+    model.load_state_dict(torch.load('model.pth'))
+    test_loss, accuracy = evaluate_model(model)
+    print(f'Test loss: {test_loss:.4f}, Accuracy: {accuracy:.4f}')
+
+if __name__ == "__main__":
+    test_model()
